2bd9c7fd0549fd1d9d5f7cc0646a26f6afeb247a
justin0u0 <mail@justin0u0.com>
2020-10-22 20:14:12 +0800

    format document

--- coff2noff/coff2noff.c
+++ coff2noff/coff2noff.c
@@ -42,7 +42,7 @@
  */
 
 #define MAIN
-#include "copyright.h" 
+#include "copyright.h"
 #undef MAIN
 
 #include <sys/types.h>
@@ -64,28 +64,30 @@
  */
 
 unsigned int
-WordToHost(unsigned int word) {
+WordToHost(unsigned int word)
+{
 #ifdef HOST_IS_BIG_ENDIAN
-	 register unsigned long result;
-	 result = (word >> 24) & 0x000000ff;
-	 result |= (word >> 8) & 0x0000ff00;
-	 result |= (word << 8) & 0x00ff0000;
-	 result |= (word << 24) & 0xff000000;
-	 return result;
-#else 
-	 return word;
+  register unsigned long result;
+  result = (word >> 24) & 0x000000ff;
+  result |= (word >> 8) & 0x0000ff00;
+  result |= (word << 8) & 0x00ff0000;
+  result |= (word << 24) & 0xff000000;
+  return result;
+#else
+  return word;
 #endif /* HOST_IS_BIG_ENDIAN */
 }
 
 unsigned short
-ShortToHost(unsigned short shortword) {
+ShortToHost(unsigned short shortword)
+{
 #if HOST_IS_BIG_ENDIAN
-	 register unsigned short result;
-	 result = (shortword << 8) & 0xff00;
-	 result |= (shortword >> 8) & 0x00ff;
-	 return result;
-#else 
-	 return shortword;
+  register unsigned short result;
+  result = (shortword << 8) & 0xff00;
+  result |= (shortword >> 8) & 0x00ff;
+  return result;
+#else
+  return shortword;
 #endif /* HOST_IS_BIG_ENDIAN */
 }
 
@@ -100,201 +102,224 @@ ShortToMachine(unsigned short shortword) { return ShortToHost(shortword); }
 // the NOFF header to little-endian format
 // on a little-endian machine, where the header is already
 // in little-endian format, it does nothing
-static void 
-SwapHeader (NoffHeader *noffH)
+static void
+SwapHeader(NoffHeader *noffH)
 {
-    noffH->noffMagic = WordToHost(noffH->noffMagic);
-    noffH->code.size = WordToHost(noffH->code.size);
-    noffH->code.virtualAddr = WordToHost(noffH->code.virtualAddr);
-    noffH->code.inFileAddr = WordToHost(noffH->code.inFileAddr);
+  noffH->noffMagic = WordToHost(noffH->noffMagic);
+  noffH->code.size = WordToHost(noffH->code.size);
+  noffH->code.virtualAddr = WordToHost(noffH->code.virtualAddr);
+  noffH->code.inFileAddr = WordToHost(noffH->code.inFileAddr);
 #ifdef RDATA
-    noffH->readonlyData.size = WordToHost(noffH->readonlyData.size);
-    noffH->readonlyData.virtualAddr = 
-           WordToHost(noffH->readonlyData.virtualAddr);
-    noffH->readonlyData.inFileAddr = 
-           WordToHost(noffH->readonlyData.inFileAddr);
+  noffH->readonlyData.size = WordToHost(noffH->readonlyData.size);
+  noffH->readonlyData.virtualAddr =
+      WordToHost(noffH->readonlyData.virtualAddr);
+  noffH->readonlyData.inFileAddr =
+      WordToHost(noffH->readonlyData.inFileAddr);
 #endif
-    noffH->initData.size = WordToHost(noffH->initData.size);
-    noffH->initData.virtualAddr = WordToHost(noffH->initData.virtualAddr);
-    noffH->initData.inFileAddr = WordToHost(noffH->initData.inFileAddr);
-    noffH->uninitData.size = WordToHost(noffH->uninitData.size);
-    noffH->uninitData.virtualAddr = WordToHost(noffH->uninitData.virtualAddr);
-    noffH->uninitData.inFileAddr = WordToHost(noffH->uninitData.inFileAddr);
+  noffH->initData.size = WordToHost(noffH->initData.size);
+  noffH->initData.virtualAddr = WordToHost(noffH->initData.virtualAddr);
+  noffH->initData.inFileAddr = WordToHost(noffH->initData.inFileAddr);
+  noffH->uninitData.size = WordToHost(noffH->uninitData.size);
+  noffH->uninitData.virtualAddr = WordToHost(noffH->uninitData.virtualAddr);
+  noffH->uninitData.inFileAddr = WordToHost(noffH->uninitData.inFileAddr);
 }
 
 /****************************************************************/
 
-#define ReadStruct(f,s) 	Read(f,(char *)&s,sizeof(s))
+#define ReadStruct(f, s) Read(f, (char *)&s, sizeof(s))
 
 char *noffFileName = NULL;
 
 /* read and check for error */
 void Read(int fd, char *buf, int nBytes)
 {
-    if (read(fd, buf, nBytes) != nBytes) {
-        fprintf(stderr, "File is too short\n");
-	unlink(noffFileName);
-	exit(1);
-    }
+  if (read(fd, buf, nBytes) != nBytes)
+  {
+    fprintf(stderr, "File is too short\n");
+    unlink(noffFileName);
+    exit(1);
+  }
 }
 
 /* write and check for error */
 void Write(int fd, char *buf, int nBytes)
 {
-    if (write(fd, buf, nBytes) != nBytes) {
-	fprintf(stderr, "Unable to write file\n");
-	unlink(noffFileName);
-	exit(1);
-    }
+  if (write(fd, buf, nBytes) != nBytes)
+  {
+    fprintf(stderr, "Unable to write file\n");
+    unlink(noffFileName);
+    exit(1);
+  }
 }
 
 int main(int argc, char **argv)
 {
-    int fdIn, fdOut, numsections, i, inNoffFile;
-    struct filehdr fileh;
-    struct aouthdr systemh;
-    struct scnhdr *sections;
-    char *buffer;
-    NoffHeader noffH;
+  int fdIn, fdOut, numsections, i, inNoffFile;
+  struct filehdr fileh;
+  struct aouthdr systemh;
+  struct scnhdr *sections;
+  char *buffer;
+  NoffHeader noffH;
 
-    if (argc < 2) {
-	fprintf(stderr, "Usage: %s <coffFileName> <noffFileName>\n", argv[0]);
-	exit(1);
-    }
-    
-/* open the COFF file (input) */
-    fdIn = open(argv[1], O_RDONLY, 0);
-    if (fdIn == -1) {
-	perror(argv[1]);
-	exit(1);
-    }
+  if (argc < 2)
+  {
+    fprintf(stderr, "Usage: %s <coffFileName> <noffFileName>\n", argv[0]);
+    exit(1);
+  }
 
-/* open the NOFF file (output) */
-    fdOut = open(argv[2], O_WRONLY|O_CREAT|O_TRUNC , 0666);
-    if (fdIn == -1) {
-	perror(argv[2]);
-	exit(1);
-    }
-    noffFileName = argv[2];
-    
-/* Read in the file header and check the magic number. */
-    ReadStruct(fdIn,fileh);
-    fileh.f_magic = ShortToHost(fileh.f_magic);
-    fileh.f_nscns = ShortToHost(fileh.f_nscns); 
-    if (fileh.f_magic != MIPSELMAGIC) {
-	fprintf(stderr, "File is not a MIPSEL COFF file\n");
-        unlink(noffFileName);
-	exit(1);
-    }
-    
-/* Read in the system header and check the magic number */
-    ReadStruct(fdIn,systemh);
-    systemh.magic = ShortToHost(systemh.magic);
-    if (systemh.magic != OMAGIC) {
-	fprintf(stderr, "File is not a OMAGIC file\n");
-        unlink(noffFileName);
-	exit(1);
-    }
-    
-/* Read in the section headers. */
-    numsections = fileh.f_nscns;
-    printf("numsections %d \n",numsections);
-    sections = (struct scnhdr *)malloc(numsections * sizeof(struct scnhdr));
-    Read(fdIn, (char *) sections, numsections * sizeof(struct scnhdr));
+  /* open the COFF file (input) */
+  fdIn = open(argv[1], O_RDONLY, 0);
+  if (fdIn == -1)
+  {
+    perror(argv[1]);
+    exit(1);
+  }
 
-   for (i = 0; i < numsections; i++) {
-     sections[i].s_paddr =  WordToHost(sections[i].s_paddr);
-     sections[i].s_size = WordToHost(sections[i].s_size);
-     sections[i].s_scnptr = WordToHost(sections[i].s_scnptr);
-   }
+  /* open the NOFF file (output) */
+  fdOut = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+  if (fdIn == -1)
+  {
+    perror(argv[2]);
+    exit(1);
+  }
+  noffFileName = argv[2];
 
- /* initialize the NOFF header, in case not all the segments are defined
+  /* Read in the file header and check the magic number. */
+  ReadStruct(fdIn, fileh);
+  fileh.f_magic = ShortToHost(fileh.f_magic);
+  fileh.f_nscns = ShortToHost(fileh.f_nscns);
+  if (fileh.f_magic != MIPSELMAGIC)
+  {
+    fprintf(stderr, "File is not a MIPSEL COFF file\n");
+    unlink(noffFileName);
+    exit(1);
+  }
+
+  /* Read in the system header and check the magic number */
+  ReadStruct(fdIn, systemh);
+  systemh.magic = ShortToHost(systemh.magic);
+  if (systemh.magic != OMAGIC)
+  {
+    fprintf(stderr, "File is not a OMAGIC file\n");
+    unlink(noffFileName);
+    exit(1);
+  }
+
+  /* Read in the section headers. */
+  numsections = fileh.f_nscns;
+  printf("numsections %d \n", numsections);
+  sections = (struct scnhdr *)malloc(numsections * sizeof(struct scnhdr));
+  Read(fdIn, (char *)sections, numsections * sizeof(struct scnhdr));
+
+  for (i = 0; i < numsections; i++)
+  {
+    sections[i].s_paddr = WordToHost(sections[i].s_paddr);
+    sections[i].s_size = WordToHost(sections[i].s_size);
+    sections[i].s_scnptr = WordToHost(sections[i].s_scnptr);
+  }
+
+  /* initialize the NOFF header, in case not all the segments are defined
   * in the COFF file
   */
-    noffH.noffMagic = NOFFMAGIC;
-    noffH.code.size = 0;
-    noffH.initData.size = 0;
-    noffH.uninitData.size = 0;
+  noffH.noffMagic = NOFFMAGIC;
+  noffH.code.size = 0;
+  noffH.initData.size = 0;
+  noffH.uninitData.size = 0;
 #ifdef RDATA
-    noffH.readonlyData.size = 0;
+  noffH.readonlyData.size = 0;
 #endif
 
+  /* Copy the segments in */
+  inNoffFile = sizeof(NoffHeader);
+  lseek(fdOut, inNoffFile, 0);
+  printf("Loading %d sections:\n", numsections);
+  for (i = 0; i < numsections; i++)
+  {
+    printf("\t\"%s\", filepos 0x%x, mempos 0x%x, size 0x%x\n",
+           sections[i].s_name, (unsigned int)sections[i].s_scnptr,
+           (unsigned int)sections[i].s_paddr, (unsigned int)sections[i].s_size);
+    if (sections[i].s_size == 0)
+    {
+      /* do nothing! */
+    }
+    else if (!strcmp(sections[i].s_name, ".text"))
+    {
+      noffH.code.virtualAddr = sections[i].s_paddr;
+      noffH.code.inFileAddr = inNoffFile;
+      noffH.code.size = sections[i].s_size;
+      lseek(fdIn, sections[i].s_scnptr, 0);
+      buffer = malloc(sections[i].s_size);
+      Read(fdIn, buffer, sections[i].s_size);
+      Write(fdOut, buffer, sections[i].s_size);
+      free(buffer);
+      inNoffFile += sections[i].s_size;
+    }
+    else if (!strcmp(sections[i].s_name, ".data"))
+    {
 
- /* Copy the segments in */
-    inNoffFile = sizeof(NoffHeader);
-    lseek(fdOut, inNoffFile, 0);
-    printf("Loading %d sections:\n", numsections);
-    for (i = 0; i < numsections; i++) {
-	printf("\t\"%s\", filepos 0x%x, mempos 0x%x, size 0x%x\n",
-	      sections[i].s_name, (unsigned int)sections[i].s_scnptr,
-	      (unsigned int)sections[i].s_paddr, (unsigned int)sections[i].s_size);
-	if (sections[i].s_size == 0) {
-		/* do nothing! */	
-	} else if (!strcmp(sections[i].s_name, ".text")) {
-	    noffH.code.virtualAddr = sections[i].s_paddr;
-	    noffH.code.inFileAddr = inNoffFile;
-	    noffH.code.size = sections[i].s_size;
-    	    lseek(fdIn, sections[i].s_scnptr, 0);
-    	    buffer = malloc(sections[i].s_size);
-    	    Read(fdIn, buffer, sections[i].s_size);
-    	    Write(fdOut, buffer, sections[i].s_size);
-    	    free(buffer);
-	    inNoffFile += sections[i].s_size;
- 	} else if (!strcmp(sections[i].s_name, ".data")){
-
-	    noffH.initData.virtualAddr = sections[i].s_paddr;
-	    noffH.initData.inFileAddr = inNoffFile;
-	    noffH.initData.size = sections[i].s_size;
-	    lseek(fdIn, sections[i].s_scnptr, 0);
-	    buffer = malloc(sections[i].s_size);
-	    Read(fdIn, buffer, sections[i].s_size);
-	    Write(fdOut, buffer, sections[i].s_size);
-	    free(buffer);
-	    inNoffFile += sections[i].s_size;
+      noffH.initData.virtualAddr = sections[i].s_paddr;
+      noffH.initData.inFileAddr = inNoffFile;
+      noffH.initData.size = sections[i].s_size;
+      lseek(fdIn, sections[i].s_scnptr, 0);
+      buffer = malloc(sections[i].s_size);
+      Read(fdIn, buffer, sections[i].s_size);
+      Write(fdOut, buffer, sections[i].s_size);
+      free(buffer);
+      inNoffFile += sections[i].s_size;
 #ifdef RDATA
-	} else if (!strcmp(sections[i].s_name, ".rdata")){
+    }
+    else if (!strcmp(sections[i].s_name, ".rdata"))
+    {
 
-	    noffH.readonlyData.virtualAddr = sections[i].s_paddr;
-	    noffH.readonlyData.inFileAddr = inNoffFile;
-	    noffH.readonlyData.size = sections[i].s_size;
-	    lseek(fdIn, sections[i].s_scnptr, 0);
-	    buffer = malloc(sections[i].s_size);
-	    Read(fdIn, buffer, sections[i].s_size);
-	    Write(fdOut, buffer, sections[i].s_size);
-	    free(buffer);
-	    inNoffFile += sections[i].s_size;
+      noffH.readonlyData.virtualAddr = sections[i].s_paddr;
+      noffH.readonlyData.inFileAddr = inNoffFile;
+      noffH.readonlyData.size = sections[i].s_size;
+      lseek(fdIn, sections[i].s_scnptr, 0);
+      buffer = malloc(sections[i].s_size);
+      Read(fdIn, buffer, sections[i].s_size);
+      Write(fdOut, buffer, sections[i].s_size);
+      free(buffer);
+      inNoffFile += sections[i].s_size;
 #endif
-	} else if (!strcmp(sections[i].s_name, ".bss")){
-  	    /* need to check if we have both .bss and .sbss -- make sure they 
+    }
+    else if (!strcmp(sections[i].s_name, ".bss"))
+    {
+      /* need to check if we have both .bss and .sbss -- make sure they 
 	     * are contiguous
 	     */
-	    if (noffH.uninitData.size != 0) {
-	        if (sections[i].s_paddr == (noffH.uninitData.virtualAddr +
-	        				noffH.uninitData.size)) {
-		    fprintf(stderr, "Can't handle both bss and sbss\n");
-		    unlink(noffFileName);
-		    exit(1);
-		}
-	        noffH.uninitData.size += sections[i].s_size;
-	    } else {
-	        noffH.uninitData.virtualAddr = sections[i].s_paddr;
-	        noffH.uninitData.size = sections[i].s_size;
-	    }
-	    /* we don't need to copy the uninitialized data! */
-	} else {
-	    fprintf(stderr, "Unknown segment type: %s\n", sections[i].s_name);
-            unlink(noffFileName);
-	    exit(1);
-	}
+      if (noffH.uninitData.size != 0)
+      {
+        if (sections[i].s_paddr == (noffH.uninitData.virtualAddr +
+                                    noffH.uninitData.size))
+        {
+          fprintf(stderr, "Can't handle both bss and sbss\n");
+          unlink(noffFileName);
+          exit(1);
+        }
+        noffH.uninitData.size += sections[i].s_size;
+      }
+      else
+      {
+        noffH.uninitData.virtualAddr = sections[i].s_paddr;
+        noffH.uninitData.size = sections[i].s_size;
+      }
+      /* we don't need to copy the uninitialized data! */
     }
-    lseek(fdOut, 0, 0);
+    else
+    {
+      fprintf(stderr, "Unknown segment type: %s\n", sections[i].s_name);
+      unlink(noffFileName);
+      exit(1);
+    }
+  }
+  lseek(fdOut, 0, 0);
 
-    // convert the NOFF header to little-endian before
-    // writing it to the file
-    SwapHeader(&noffH);
-    
-    Write(fdOut, (char *)&noffH, sizeof(NoffHeader));
-    close(fdIn);
-    close(fdOut);
-    exit(0);
+  // convert the NOFF header to little-endian before
+  // writing it to the file
+  SwapHeader(&noffH);
+
+  Write(fdOut, (char *)&noffH, sizeof(NoffHeader));
+  close(fdIn);
+  close(fdOut);
+  exit(0);
 }
