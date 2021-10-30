1914c769970513fc314fc1b1f8085f557489fcef
justin0u0 <mail@justin0u0.com>
2021-10-30 15:13:19 +0800

    style: format code

--- code/threads/alarm.cc
+++ code/threads/alarm.cc
@@ -5,24 +5,24 @@
 //	Not completely implemented.
 //
 // Copyright (c) 1992-1996 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation 
+// All rights reserved.  See copyright.h for copyright notice and limitation
 // of liability and disclaimer of warranty provisions.
 
-#include "copyright.h"
 #include "alarm.h"
+
+#include "copyright.h"
 #include "main.h"
 
 //----------------------------------------------------------------------
 // Alarm::Alarm
 //      Initialize a software alarm clock.  Start up a timer device
 //
-//      "doRandom" -- if true, arrange for the hardware interrupts to 
+//      "doRandom" -- if true, arrange for the hardware interrupts to
 //		occur at random, instead of fixed, intervals.
 //----------------------------------------------------------------------
 
-Alarm::Alarm(bool doRandom)
-{
-    timer = new Timer(doRandom, this);
+Alarm::Alarm(bool doRandom) {
+  timer = new Timer(doRandom, this);
 }
 
 //----------------------------------------------------------------------
@@ -35,21 +35,19 @@ Alarm::Alarm(bool doRandom)
 //	Note that instead of calling Yield() directly (which would
 //	suspend the interrupt handler, not the interrupted thread
 //	which is what we wanted to context switch), we set a flag
-//	so that once the interrupt handler is done, it will appear as 
-//	if the interrupted thread called Yield at the point it is 
+//	so that once the interrupt handler is done, it will appear as
+//	if the interrupted thread called Yield at the point it is
 //	was interrupted.
 //
-//	For now, just provide time-slicing.  Only need to time slice 
+//	For now, just provide time-slicing.  Only need to time slice
 //      if we're currently running something (in other words, not idle).
 //----------------------------------------------------------------------
 
-void 
-Alarm::CallBack() 
-{
-    Interrupt *interrupt = kernel->interrupt;
-    MachineStatus status = interrupt->getStatus();
-    
-    if (status != IdleMode) {
-	interrupt->YieldOnReturn();
-    }
+void Alarm::CallBack() {
+  Interrupt *interrupt = kernel->interrupt;
+  MachineStatus status = interrupt->getStatus();
+
+  if (status != IdleMode) {
+    interrupt->YieldOnReturn();
+  }
 }
